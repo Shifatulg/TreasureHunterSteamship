be1996d6f1c9d9a2a49613da42fc494687fae983
BT_1N3_10 <Yuhaog2@nycstudents.net>
2023-12-18 08:51:03 -0500

    Cloner testing -Shifatulfsddsfdsf

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -2,6 +2,6 @@ public class TreasureHunterRunner {
     public static void main(String[] args) {
         TreasureHunter game = new TreasureHunter();
         game.play();
-        System.out.println("Shifu here");
+        System.out.println("Shifu herfdge");
     }
 }
