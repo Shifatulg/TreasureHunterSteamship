3eff23555c0c5f34c85bb98937f4b405085d1d74
BT_1N3_33 <Yuhaog2@nycstudents.net>
2023-12-19 09:31:57 -0500

    shovel in progress

--- src/TreasureHunter.java
+++ src/TreasureHunter.java
@@ -112,6 +112,7 @@ public class TreasureHunter {
             System.out.println("(M)ove on to a different town.");
             System.out.println("(L)ook for trouble!");
             System.out.println("Give up the hunt and e(X)it.");
+            System.out.println("(D)ig for gold!");
             System.out.println();
             System.out.print("What's your next move? ");
             choice = SCANNER.nextLine().toLowerCase();
@@ -141,8 +142,21 @@ public class TreasureHunter {
             currentTown.lookForTrouble();
         } else if (choice.equals("x")) {
             System.out.println("Fare thee well, " + hunter.getHunterName() + "!");
-        } else {
+        } else if (choice.equals("d")){
+            if(Shop.HaveShovel){
+                double rnd = Math.random();
+                if(rnd>.5){
+                    System.out.println("You dug but only found dirt");
+                }else{
+
+                }
+            }
+        }
+        else {
             System.out.println("Yikes! That's an invalid option! Try again.");
         }
+    }
+    public int getGold(){
+        return (int)(Math.random()*20)+1;
     }
 }
