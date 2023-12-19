a57953292656768908398c298973be53993d7565
BT_1N3_11 <sd@dsd.com>
2023-12-19 08:35:58 -0500

    able to lose -siheng

--- src/Town.java
+++ src/Town.java
@@ -109,6 +109,9 @@ public class Town {
                 printMessage += "That'll teach you to go lookin' fer trouble in MY town! Now pay up!";
                 printMessage += "\nYou lost the brawl and pay " +Colors.YELLOW+ goldDiff +Colors.RESET+ " gold.";
                 hunter.changeGold(-goldDiff);
+                if (hunter.getGold() < 0) {
+                    TreasureHunter.lose();
+                }
             }
         }
     }
